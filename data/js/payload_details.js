63305955e437e909338940624f60c61653c88bb5
benjam1nm <benjamin.m@gmail.com>
2018-03-04 20:18:12 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38032,
-	"created_at": "Thu Mar 01 01:18:12 +0000 2018",
+	"tweets": 38033,
+	"created_at": "Mon Mar 05 01:18:11 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_03.js
@@ -0,0 +1,236 @@
+Grailbird.data.tweets_2018_03 = 
+[
+	{
+		"created_at": "Sun Mar 04 17:11:49 +0000 2018",
+		"id": 970346083007287300,
+		"id_str": "970346083007287298",
+		"text": "RT @RevEverett: 7 dead across the northeast from this massive storm. Climate change already killing. https://t.co/ieXMbWfzhV",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RevEverett",
+					"name": "Laura Everett",
+					"id": 415786964,
+					"id_str": "415786964",
+					"indices": [
+						3,
+						14
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ieXMbWfzhV",
+					"expanded_url": "https://www.bostonglobe.com/2018/03/03/northeast-seven-dead-including-two-children-fierce-storm/2OTGs5frMecfAP0uYyV7KP/story.html",
+					"display_url": "bostonglobe.com/2018/03/03/nor…",
+					"indices": [
+						101,
+						124
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 28883923,
+			"id_str": "28883923",
+			"name": "Benjamin M.",
+			"screen_name": "wanderingBAM",
+			"location": "",
+			"description": "Partner of @BFGtweets, lapsed kayaker, home cook, Scrabble enthusiast, Canadian south of the border.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1964,
+			"friends_count": 1157,
+			"listed_count": 82,
+			"created_at": "Sat Apr 04 22:25:26 +0000 2009",
+			"favourites_count": 546,
+			"utc_offset": -18000,
+			"time_zone": "Eastern Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 37943,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "7CA653",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/493494164391096321/g8CgygkX.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/493494164391096321/g8CgygkX.jpeg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/632245363999641600/hVDzpas7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/632245363999641600/hVDzpas7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/28883923/1360851202",
+			"profile_link_color": "7C7C7C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC66",
+			"profile_text_color": "807540",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Mar 04 14:04:51 +0000 2018",
+			"id": 970299034459045900,
+			"id_str": "970299034459045888",
+			"text": "7 dead across the northeast from this massive storm. Climate change already killing. https://t.co/ieXMbWfzhV",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ieXMbWfzhV",
+						"expanded_url": "https://www.bostonglobe.com/2018/03/03/northeast-seven-dead-including-two-children-fierce-storm/2OTGs5frMecfAP0uYyV7KP/story.html",
+						"display_url": "bostonglobe.com/2018/03/03/nor…",
+						"indices": [
+							85,
+							108
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 415786964,
+				"id_str": "415786964",
+				"name": "Laura Everett",
+				"screen_name": "RevEverett",
+				"location": "Boston, MA",
+				"description": "Let's all get free. Unabashed urbanist/bicycle evangelist. #HolySpokes book. #CanTheseBones podcast. Exec. @MassChurches. Putting the protest back in Protestant",
+				"url": "https://t.co/h9ubYwaNlE",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/h9ubYwaNlE",
+								"expanded_url": "http://RevEverett.com",
+								"display_url": "RevEverett.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4771,
+				"friends_count": 3416,
+				"listed_count": 220,
+				"created_at": "Fri Nov 18 20:26:26 +0000 2011",
+				"favourites_count": 24364,
+				"utc_offset": -14400,
+				"time_zone": "Atlantic Time (Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 30060,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/839504370056085504/wWkWsDgA_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/839504370056085504/wWkWsDgA_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/415786964/1484111299",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "67b98f17fdcf20be",
+				"url": "https://api.twitter.com/1.1/geo/id/67b98f17fdcf20be.json",
+				"place_type": "city",
+				"name": "Boston",
+				"full_name": "Boston, MA",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-71.191421,
+								42.227797
+							],
+							[
+								-70.986004,
+								42.227797
+							],
+							[
+								-70.986004,
+								42.399542
+							],
+							[
+								-71.191421,
+								42.399542
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
