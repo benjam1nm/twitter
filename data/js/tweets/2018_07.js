04ca2bd212b991da9b0b68c858c5b6c0a004747e
benjam1nm <benjamin.m@gmail.com>
2018-07-07 21:18:11 -0400

    Added 1 tweets

---
+++ data/js/tweets/2018_07.js
@@ -0,0 +1,111 @@
+Grailbird.data.tweets_2018_07 = 
+[
+	{
+		"created_at": "Sat Jul 07 23:55:30 +0000 2018",
+		"id": 1015746160080932900,
+		"id_str": "1015746160080932864",
+		"text": "@itsdlevy I kid you not, @BFGtweets has been talking about getting this non-stop foreverrrrr. The ad stalks her on… https://t.co/X05wL5FaAb",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "itsdlevy",
+					"name": "it's delightful, it's delicious, it's dlevy!",
+					"id": 30719281,
+					"id_str": "30719281",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "BFGtweets",
+					"name": "Stephanie",
+					"id": 177952885,
+					"id_str": "177952885",
+					"indices": [
+						25,
+						35
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/X05wL5FaAb",
+					"expanded_url": "https://twitter.com/i/web/status/1015746160080932864",
+					"display_url": "twitter.com/i/web/status/1…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1014873784396976100,
+		"in_reply_to_status_id_str": "1014873784396976129",
+		"in_reply_to_user_id": 30719281,
+		"in_reply_to_user_id_str": "30719281",
+		"in_reply_to_screen_name": "itsdlevy",
+		"user": {
+			"id": 28883923,
+			"id_str": "28883923",
+			"name": "Benjamin M.",
+			"screen_name": "wanderingBAM",
+			"location": "",
+			"description": "Partner of @BFGtweets, lapsed kayaker, home cook, Scrabble enthusiast, Canadian south of the border.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1935,
+			"friends_count": 1157,
+			"listed_count": 80,
+			"created_at": "Sat Apr 04 22:25:26 +0000 2009",
+			"favourites_count": 551,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 37930,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "7CA653",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/632245363999641600/hVDzpas7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/632245363999641600/hVDzpas7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/28883923/1360851202",
+			"profile_link_color": "7C7C7C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC66",
+			"profile_text_color": "807540",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	}
+]
