389be7ba18d955ea563837983e252db39de27bfa
benjam1nm <benjamin.m@gmail.com>
2019-09-09 21:18:12 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38053,
-	"created_at": "Fri Dec 14 01:18:10 +0000 2018",
+	"tweets": 38054,
+	"created_at": "Tue Sep 10 01:18:11 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_09.js
@@ -0,0 +1,495 @@
+Grailbird.data.tweets_2019_09 = 
+[
+	{
+		"created_at": "Mon Sep 09 18:15:42 +0000 2019",
+		"id": 1171125040294834200,
+		"id_str": "1171125040294834176",
+		"full_text": "@WCVB @universalhub This: https://t.co/Q3Ceyv5QJJ",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			49
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "WCVB",
+					"name": "WCVB-TV Boston",
+					"id": 21419504,
+					"id_str": "21419504",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "universalhub",
+					"name": "Adam Gaffin",
+					"id": 12705772,
+					"id_str": "12705772",
+					"indices": [
+						6,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Q3Ceyv5QJJ",
+					"expanded_url": "https://twitter.com/LiveBoston617/status/1171108125837017088",
+					"display_url": "twitter.com/LiveBoston617/…",
+					"indices": [
+						26,
+						49
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1171123028979323000,
+		"in_reply_to_status_id_str": "1171123028979322880",
+		"in_reply_to_user_id": 28883923,
+		"in_reply_to_user_id_str": "28883923",
+		"in_reply_to_screen_name": "wanderingBAM",
+		"user": {
+			"id": 28883923,
+			"id_str": "28883923",
+			"name": "Benjamin M.",
+			"screen_name": "wanderingBAM",
+			"location": "",
+			"description": "Partner of @BFGtweets, lapsed kayaker, home cook, Scrabble enthusiast, Canadian south of the border.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 1843,
+			"friends_count": 1165,
+			"listed_count": 76,
+			"created_at": "Sat Apr 04 22:25:26 +0000 2009",
+			"favourites_count": 489,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 37538,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "7CA653",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/632245363999641600/hVDzpas7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/632245363999641600/hVDzpas7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/28883923/1360851202",
+			"profile_link_color": "7C7C7C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC66",
+			"profile_text_color": "807540",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1171108125837017000,
+		"quoted_status_id_str": "1171108125837017088",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Q3Ceyv5QJJ",
+			"expanded": "https://twitter.com/LiveBoston617/status/1171108125837017088",
+			"display": "twitter.com/LiveBoston617/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Sep 09 17:08:29 +0000 2019",
+			"id": 1171108125837017000,
+			"id_str": "1171108125837017088",
+			"full_text": "🛑ACTIVE SCENE🛑\n\n#MSP &amp; #MBTAPD on scene for the death investigation on the #Neponset Bike Path behind the #MBTA #Mattapan Station. \n\nInvestigators on scene interviewing bystanders and collecting evidence. \n\n13:00 hours #BOSTON #Live #Now https://t.co/g3KdnFLqJs",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				241
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MSP",
+						"indices": [
+							16,
+							20
+						]
+					},
+					{
+						"text": "MBTAPD",
+						"indices": [
+							27,
+							34
+						]
+					},
+					{
+						"text": "Neponset",
+						"indices": [
+							79,
+							88
+						]
+					},
+					{
+						"text": "MBTA",
+						"indices": [
+							110,
+							115
+						]
+					},
+					{
+						"text": "Mattapan",
+						"indices": [
+							116,
+							125
+						]
+					},
+					{
+						"text": "BOSTON",
+						"indices": [
+							223,
+							230
+						]
+					},
+					{
+						"text": "Live",
+						"indices": [
+							231,
+							236
+						]
+					},
+					{
+						"text": "Now",
+						"indices": [
+							237,
+							241
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1171108086725132300,
+						"id_str": "1171108086725132288",
+						"indices": [
+							242,
+							265
+						],
+						"media_url": "http://pbs.twimg.com/media/EECcmZ7WwAAxFvo.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EECcmZ7WwAAxFvo.jpg",
+						"url": "https://t.co/g3KdnFLqJs",
+						"display_url": "pic.twitter.com/g3KdnFLqJs",
+						"expanded_url": "https://twitter.com/LiveBoston617/status/1171108125837017088/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1364,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 799,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1171108086725132300,
+						"id_str": "1171108086725132288",
+						"indices": [
+							242,
+							265
+						],
+						"media_url": "http://pbs.twimg.com/media/EECcmZ7WwAAxFvo.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EECcmZ7WwAAxFvo.jpg",
+						"url": "https://t.co/g3KdnFLqJs",
+						"display_url": "pic.twitter.com/g3KdnFLqJs",
+						"expanded_url": "https://twitter.com/LiveBoston617/status/1171108125837017088/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1364,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 799,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1171108086972596200,
+						"id_str": "1171108086972596224",
+						"indices": [
+							242,
+							265
+						],
+						"media_url": "http://pbs.twimg.com/media/EECcma2WwAA0Wbp.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EECcma2WwAA0Wbp.jpg",
+						"url": "https://t.co/g3KdnFLqJs",
+						"display_url": "pic.twitter.com/g3KdnFLqJs",
+						"expanded_url": "https://twitter.com/LiveBoston617/status/1171108125837017088/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 799,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1364,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1171108088155451400,
+						"id_str": "1171108088155451398",
+						"indices": [
+							242,
+							265
+						],
+						"media_url": "http://pbs.twimg.com/media/EECcmfQXsAY8-G9.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EECcmfQXsAY8-G9.jpg",
+						"url": "https://t.co/g3KdnFLqJs",
+						"display_url": "pic.twitter.com/g3KdnFLqJs",
+						"expanded_url": "https://twitter.com/LiveBoston617/status/1171108125837017088/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 799,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1364,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1171108094891479000,
+						"id_str": "1171108094891479040",
+						"indices": [
+							242,
+							265
+						],
+						"media_url": "http://pbs.twimg.com/media/EECcm4WXUAAEBlV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EECcm4WXUAAEBlV.jpg",
+						"url": "https://t.co/g3KdnFLqJs",
+						"display_url": "pic.twitter.com/g3KdnFLqJs",
+						"expanded_url": "https://twitter.com/LiveBoston617/status/1171108125837017088/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1364,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 799,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 851003790056620000,
+				"id_str": "851003790056620032",
+				"name": "Live Boston",
+				"screen_name": "LiveBoston617",
+				"location": "Boston, MA",
+				"description": "“@“ us with any tips! Nonprofit for the documentation & historical preservation of Boston‘s first responders. All images not to be used without express consent.",
+				"url": "https://t.co/XEwDT4hYNf",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/XEwDT4hYNf",
+								"expanded_url": "https://liveboston617.smugmug.com",
+								"display_url": "liveboston617.smugmug.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3861,
+				"friends_count": 695,
+				"listed_count": 50,
+				"created_at": "Sun Apr 09 09:28:09 +0000 2017",
+				"favourites_count": 13226,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5638,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1098142376319766528/tPjkmVHp_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1098142376319766528/tPjkmVHp_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/851003790056620032/1550653770",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "67b98f17fdcf20be",
+				"url": "https://api.twitter.com/1.1/geo/id/67b98f17fdcf20be.json",
+				"place_type": "city",
+				"name": "Boston",
+				"full_name": "Boston, MA",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-71.191421,
+								42.227797
+							],
+							[
+								-70.986004,
+								42.227797
+							],
+							[
+								-70.986004,
+								42.399542
+							],
+							[
+								-71.191421,
+								42.399542
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 11,
+			"favorite_count": 13,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@WCVB @universalhub This: https://t.co/Q3Ceyv5QJJ"
+	}
+]
